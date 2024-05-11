d04b6102cd733c37ce60dfe16ff962a73148299e
dataNerd65 <pkiarie811@gmail.com>
2024-05-11 12:39:38 +0300

    trying to add these auto gen files

--- src/com/kkiarie/mypackage/Sasii.java
+++ src/com/kkiarie/mypackage/Sasii.java
@@ -7,4 +7,22 @@ public class Sasii {
     public void printMessage() {
         System.out.println("A new instance is created to ensure first statement is executed");
     }
+    //concepts on multidimensional arrays
+    public void MultiArrays() {
+        int [] [] myNumbers = {{1, 2, 3, 4}, {5, 6, 7}};
+        System.out.println(myNumbers[1][2]);
+        myNumbers[1][2] = 23;
+        System.out.println(myNumbers[1][2]);
+        for(int[] row: myNumbers) {
+            for(int f : row){
+                System.out.println(f);
+            }
+        }
+        System.out.println("Now using for loop in multidimensional arrays.");
+        for(int g =  0; g < myNumbers.length; ++g){
+            for(int u = 0; u < myNumbers[g].length; ++u){
+                System.out.println(myNumbers[g][u]);
+            }
+        }
+    }
 }
