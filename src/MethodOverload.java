5437203c430266be5ece9bd7e2a8eb094615cc41
dataNerd65 <pkiarie811@gmail.com>
2024-05-16 10:16:30 +0300

    Learnt and implemented method overloading

--- src/MethodOverload.java
+++ src/MethodOverload.java
@@ -1,2 +1,18 @@
+//Demonstrating use of method overloading in java
 public class MethodOverload {
+    static int plusMethodInt(int x, int y){
+        return x + y;
+
+    }
+    static double plusMethodDouble(double x, double y){
+        return x + y;
+
+    }
+    public static void main(String[] args){
+        int myNum1 = plusMethodInt(8, 5);
+        double myNum2 = plusMethodDouble(4.3, 6.26);
+
+        System.out.println("Int: " + myNum1);
+        System.out.println("Double: " + myNum2);
+    }
 }
